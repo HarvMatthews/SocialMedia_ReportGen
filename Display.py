b68f0bbe75b1485c6b801cd5db5fe746295053d0
HarvMatthews <Harveymjm15@gmail.com>
2025-03-06 18:13:51 +0000

    Created Design Idea

--- Display.py
+++ Display.py
@@ -1,14 +1,13 @@
 from reportlab.pdfgen import canvas
 
 def hello(canvas):
-    canvas.drawRightString(400,100,"ss World")
+    canvas.drawString(450,100,"WHAT IF I DO A REALLU LONG MESSAGE< AND DOES IT GO OFF PAGE?! World")
     canvas.line(0,0,595,842)
     
     path = canvas.beginPath()
     path.rect(100,200,150,90)
     canvas.drawPath(path, stroke=1, fill=1, fillMode=None) 
 
-canvas.drawImage(self, image, 20,20, width=None,height=None,mask=None) 
 
 
 c = canvas.Canvas("hello.pdf")
